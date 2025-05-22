593b338d843c0667d9a5df2911ef6937618f8823
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-05-22 16:49:03 -0300

    Refactor code structure for improved readability and maintainability

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,45 @@
+import { ShoppingCartIcon } from '@phosphor-icons/react'
+import coffee from '../../assets/coffee.svg'
+
 export function Home() {
-  return <div>oioioi</div>
+  return (
+    <section>
+      <div>
+        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+        <p>
+          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+          hora
+        </p>
+        <div>
+          <div>
+            <div>
+              <ShoppingCartIcon size={32} weight="fill" />
+            </div>
+            <span>Compra simples e segura</span>
+          </div>
+          <div>
+            <div>
+              <ShoppingCartIcon size={32} weight="fill" />
+            </div>
+            <span>Compra simples e segura</span>
+          </div>
+          <div>
+            <div>
+              <ShoppingCartIcon size={32} weight="fill" />
+            </div>
+            <span>Compra simples e segura</span>
+          </div>
+          <div>
+            <div>
+              <ShoppingCartIcon size={32} weight="fill" />
+            </div>
+            <span>Compra simples e segura</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src={coffee} alt="" />
+      </div>
+    </section>
+  )
 }
